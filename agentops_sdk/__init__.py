1d7a871ff0d107da8be9b1b3be6aa84253b0e0b1
sahiee-dev <sahir150305@gmail.com>
2026-05-18 20:29:23 +0530

    feat: export ZKP differential audit functions from SDK public API

--- agentops_sdk/__init__.py
+++ agentops_sdk/__init__.py
@@ -1,2 +1,7 @@
 from agentops_sdk.client import AgentOpsClient
 from agentops_sdk.events import EventType
+from agentops_sdk.zkp_differential import (
+    commit_session_count,
+    prove_differential,
+    verify_differential,
+)
